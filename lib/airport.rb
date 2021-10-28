8519d7dffc79f43c9f2c5def5d22b2407e54759a
zainab baruud <zainabbaruud@ma373.home>
2021-10-28 08:49:09 +0100

    first test to respont to plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,7 @@
 class Airport
 
-  def initialize(capacity)
-    @capacity = capacity
-    @num_of_planes_that_land = 0
+  def land
+  
   end
-
-  # User story 1 & 3
-  def land(plane)
-    raise "Airport is full, you can't land this plane" if @num_of_planes_that_land >= @capacity
-    
-    @num_of_planes_that_land += 1
-  end
-
-  # User story 2
-  def take_off(plane)
-
-  end
+  
 end
